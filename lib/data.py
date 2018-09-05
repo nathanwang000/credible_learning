b1d2966186c3a80f5f411a8be0cc70f94fee505f
Jiaxuan Wang <nathanwang000@gmail.com>
2018-09-04 23:14:02 -0400

    added duplicate exp

--- lib/data.py
+++ lib/data.py
@@ -10,7 +10,8 @@ from sklearn.preprocessing import StandardScaler
 
 class Mimic2(Dataset):
 
-    def __init__(self, mode='dead', random_risk=False, expert_feature_only=False):
+    def __init__(self, mode='total', random_risk=False,
+                 expert_feature_only=False, duplicate=0):
         '''
         mode in [dead, survivor, total]: ways to impute missingness
         '''
@@ -21,17 +22,18 @@ class Mimic2(Dataset):
             self.x = self.x.ix[:,-16:]
         self.y = self.data['In-hospital_death']
 
-        self.xtr, self.xte, self.ytr, self.yte = train_test_split(self.x,
-                                                                  self.y,
-                                                                  test_size=0.25,
-                                                                  random_state=42,
-                                                                  stratify=self.y)
+        xtrain, self.xte, ytrain, self.yte = train_test_split(self.x,
+                                                              self.y,
+                                                              test_size=0.25,
+                                                              random_state=42,
+                                                              stratify=self.y)
 
-        self.xtrain,self.xval,self.ytrain,self.yval = train_test_split(self.xtr,
-                                                                       self.ytr,
+        self.xtrain,self.xval,self.ytrain,self.yval = train_test_split(xtrain,
+                                                                       ytrain,
                                                                        test_size=0.25,
                                                                        random_state=42,
-                                                                       stratify=self.ytr)
+                                                                       stratify=ytrain)
+
 
         # standardize model
         scaler = StandardScaler()
@@ -39,9 +41,9 @@ class Mimic2(Dataset):
         self.xtrain = scaler.transform(self.xtrain)
         self.xval = scaler.transform(self.xval)
         self.xte = scaler.transform(self.xte)  
-        self.ytrain = self.ytrain.as_matrix()
-        self.yval = self.yval.as_matrix()
-        self.yte = self.yte.as_matrix()        
+        self.ytrain = self.ytrain.values
+        self.yval = self.yval.values
+        self.yte = self.yte.values        
 
         # risk factors to use
         self.r = Variable(torch.FloatTensor(list(map(lambda name: 1 \
@@ -49,11 +51,15 @@ class Mimic2(Dataset):
                                                      self.x.columns))))
         if random_risk:
             np.random.seed(42)
-            # nones = int(sum(self.r).data[0])
-            # r = np.zeros_like(self.r.data.numpy())
-            # r[:nones] = 1
             r = np.random.permutation(self.r.data.numpy())            
             self.r = Variable(torch.from_numpy(r))
+
+        for i in range(duplicate):
+            self.xtrain = np.hstack([self.xtrain, self.xtrain])
+            self.xval = np.hstack([self.xval, self.xval])
+            self.xte = np.hstack([self.xte, self.xte])
+            self.r = torch.cat([self.r, self.r])
+                    
 
     def __len__(self):
         return len(self.data)
--- main.py
+++ main.py
@@ -156,6 +156,17 @@ def expert_feature_only_exp(n_cpus=None, n_bootstrap=30):
 
     ps.run(n_bootstrap)
 
+def duplicate_exp(regs, n_cpus=None, n_bootstrap=30):
+    m = Mimic2(mode='total', duplicate=1)
+    ps = ParamSearch(m, n_cpus)
+    
+    alphas = [0.1, 0.01, 0.001, 0.0001, 0.00001]
+    for reg in regs:
+        for alpha in alphas:
+            name = reg.__name__ + '^' + str(alpha)
+            ps.add_param(name, reg, alpha)
+
+    ps.run(n_bootstrap)
     
 #####################################################
 def wridge1_5(*args, **kwargs):
